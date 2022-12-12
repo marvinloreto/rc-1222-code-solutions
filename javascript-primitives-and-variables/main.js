a481afc9803b61684dedcbb4f914da55098d87c3
Marvin Loreto <marvinjloreto@gmail.com>
2022-12-12 18:03:41 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,29 @@
+var fullName = 'Marvin Loreto';
+
+var isCool = true;
+
+var totalPets = 0;
+
+var unknown = null;
+
+var never;
+
+console.log('Full Name:', fullName);
+
+console.log('Are they cool?', isCool);
+
+console.log('Number of pets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+
+console.log('typeof isCool:', typeof isCool);
+
+console.log('typeof totalPets:', typeof totalPets);
+
+console.log('value of unknown:', unknown);
+
+console.log('typeof unknown:', typeof unknown);
+
+console.log('value of never:', never);
+
+console.log('typeof never:', typeof never);
