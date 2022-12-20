d363e5b726589b5b9710d5491d19e693856472f9
Marvin Loreto <marvinjloreto@gmail.com>
2022-12-20 20:00:25 +0000

    Added DOM and events for email and message

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -9,12 +9,21 @@ function handleBlur(event) {
 }
 
 function handleInput(event) {
-  console.log('target name:', event.target.name);
-  console.log('target value:', event.target.value);
+  console.log(event.target.name + ':', event.target.value);
 }
 
 var userName = document.querySelector('#user-name');
+var userEmail = document.querySelector('#user-email');
+var userMessage = document.querySelector('#user-message');
 
 userName.addEventListener('focus', handleFocus);
 userName.addEventListener('blur', handleBlur);
 userName.addEventListener('input', handleInput);
+
+userEmail.addEventListener('focus', handleFocus);
+userEmail.addEventListener('blur', handleBlur);
+userEmail.addEventListener('input', handleInput);
+
+userMessage.addEventListener('focus', handleFocus);
+userMessage.addEventListener('blur', handleBlur);
+userMessage.addEventListener('input', handleInput);
