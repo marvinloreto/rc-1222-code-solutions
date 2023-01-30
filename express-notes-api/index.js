e511e4aed1725e20e7f17680c4bacc7ce7fd8845
Marvin Loreto <marvinjloreto@gmail.com>
2023-01-30 00:00:32 +0000

    Added Number method and additional checks

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -14,9 +14,9 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
+  const id = Number(req.params.id);
   const note = dataJSON.notes[id];
-  if (id < 1) {
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (note === undefined) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
@@ -50,9 +50,9 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
+  const id = Number(req.params.id);
   const note = dataJSON.notes[id];
-  if (id < 1) {
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (note === undefined) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
@@ -71,9 +71,9 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
+  const id = Number(req.params.id);
   const updateNote = req.body;
-  if (id < 1) {
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!updateNote.content) {
     res.status(400).json({ error: 'content is needed' });
