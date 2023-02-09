373fc18153adab639f87a43f976e637d2b0f633f
Marvin Loreto <marvinjloreto@gmail.com>
2023-02-09 18:29:39 +0000

    Completed react-stopwatch-component

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,6 +4,5 @@ import { Stopwatch } from './stopwatch';
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-const element = <Stopwatch />;
 
-root.render(element);
+root.render(<Stopwatch />);
